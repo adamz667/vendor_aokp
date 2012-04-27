62f7de96c20e7453cf7302330729f86c51722766
Jacek Adamski <adams@g.pl>
2012-04-28 00:32:57 +0200

    Add vision.mk to products
    Change-Id: I5525bed3cb616fea1c22dbaa8309da8c45c1f921

---
+++ products/vision.mk
@@ -0,0 +1,14 @@
+$(call inherit-product, device/htc/vision/full_vision.mk)
+
+PRODUCT_RELEASE_NAME := VISION
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/aokp/configs/common_full_phone.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/aokp/configs/gsm.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_vision BUILD_ID=IMM76I BUILD_FINGERPRINT=htc_wwe/htc_vision/vision:4.0.4/IMM76I/87995:user/release-keys PRIVATE_BUILD_DESC="4.0.4 Andromadus release-keys"
+
+PRODUCT_NAME := aokp_vision
+PRODUCT_DEVICE := vision
