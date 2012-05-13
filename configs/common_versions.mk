334bc82e4496814c9c6ac7ff05ddca12963a5360
Jacek Adamski <adams@g.pl>
2012-05-13 19:25:11 +0200

    Change product name
    Change-Id: Ib161ad88563a1530c2ac7fba065db2f339c1e902

--- configs/common_versions.mk
+++ configs/common_versions.mk
@@ -4,7 +4,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=IMM76I BUILD_VERSION_TAGS=relea
 # Rom Manager properties
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.rommanager.developerid=teamkang \
-    ro.aokp.version=$(TARGET_PRODUCT)_build-35
+    ro.aokp.version=$(TARGET_PRODUCT)_build-35_$(shell date +%Y%m%d)
 
 # Goo updater app
 PRODUCT_PROPERTY_OVERRIDES += \
