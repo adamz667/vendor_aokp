84e7843aafe53ff107215ec090ff22cf7c5d1187
Jacek Adamski <adams@g.pl>
2012-05-14 19:02:32 +0200

    Typo
    Change-Id: I99f4c0350b51fb744ce8a0d3a3249eb866631f5e

--- configs/common_phone.mk
+++ configs/common_phone.mk
@@ -1,8 +1,6 @@
 $(call inherit-product, vendor/aokp/configs/common.mk)
 
 PRODUCT_COPY_FILES += \
-    vendor/aokp/prebuilt/common/app/NovaLauncher.apk:system/app/NovaLauncher.apk
+    vendor/aokp/prebuilt/common/app/NovaLauncher.apk:system/app/NovaLauncher.apk \
     vendor/aokp/prebuilt/common/app/google_music.apk:system/app/google_music.apk
-     
-    
     
