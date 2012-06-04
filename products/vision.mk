56397a8bb6a8d54be07876ef6f6d1b089c398195
Jacek Adamski <adams@g.pl>
2012-06-04 21:49:03 +0200

    Update fingerprint + add new boot animation
    Change-Id: I64631eddf0a53f8eb845d4e7c30d92465d92e7b3

--- products/vision.mk
+++ products/vision.mk
@@ -10,10 +10,10 @@ $(call inherit-product, vendor/aokp/configs/gsm.mk)
 
 PRODUCT_PACKAGE_OVERLAYS += vendor/aokp/overlay/vision
 
-#PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=htc_wwe/htc_vision/vision:4.0.4/IMM76I/87995:user/release-keys PRIVATE_BUILD_DESC="4.0.4 AOKP release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=htc_wwe/htc_vision/vision:4.0.4/IMM76I/87995:user/release-keys PRIVATE_BUILD_DESC="4.0.4 AOKP release-keys"
 
 PRODUCT_NAME := aokp_vision
 PRODUCT_DEVICE := vision
 
 PRODUCT_COPY_FILES += \
-vendor/aokp/prebuilt/common/media/bootanimation.zip:system/media/bootanimation.zip
+        vendor/aokp/prebuilt/bootanimation/bootanimation_480_800.zip:system/media/bootanimation.zip
